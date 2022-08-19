edb2228621d109ed12cbb716fe396b50eee6eda1
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 07:03:14 -0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -17,39 +17,3 @@ node send.js v output.mp4
 echo 'Video sended sucessfully :)'
 
 rm output.mp4
-
-
-
-echo 'Auto Video making and upload it to Telegram'
-
-echo 'Creating video ...'
-
-python create.py --mel
-
-echo 'Video Created :)'
-
-echo 'Sending to Telegram ...'
-
-node send.js v output.mp4
-
-echo 'Video sended sucessfully :)'
-
-rm output.mp4
-
-
-
-echo 'Auto Video making and upload it to Telegram'
-
-echo 'Creating video ...'
-
-python create.py --mot
-
-echo 'Video Created :)'
-
-echo 'Sending to Telegram ...'
-
-node send.js v output.mp4
-
-echo 'Video sended sucessfully :)'
-
-rm output.mp4
