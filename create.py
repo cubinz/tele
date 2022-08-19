fd7776defd412152a909b7cc93484abc662dc581
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 08:58:37 -0700

    Update create.py

--- create.py
+++ create.py
@@ -20,8 +20,8 @@ def download_required_images_and_audio(query):
     else:
         image = peakpx(query)[random.randint(0, 10)]
         save_images([image], "pictures")
-        image = peakpx2(query)[random.randint(20, 30)]
-        save_images2([image], "pictures",2)        
+        image2 = peakpx(query)[random.randint(20, 30)]
+        save_images2([image2], "pictures",2)        
         download_mp3(Genre.mass, random.randint(0, 20))
 
 
