dc84a775ef94b3f1e22cfbf5ffe85d79c2f422a9
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 04:04:53 -0700

    Update audio.py

--- src/audio.py
+++ src/audio.py
@@ -10,11 +10,11 @@ class Genre(Enum):
 def download_mp3(type: Genre, id: int):
 
     if type == Genre.mass:
-        target_playlist = "https://www.youtube.com/playlist?list=PLti4gdOly2nnCRZEibywyX9CoTiN2IZ5s"
+        target_playlist = "https://www.youtube.com/playlist?list=PLEF3AF2D56CBF2DC2"
     elif type == Genre.melody:
-        target_playlist = "https://www.youtube.com/playlist?list=PLti4gdOly2nnCRZEibywyX9CoTiN2IZ5s"
+        target_playlist = "https://www.youtube.com/playlist?list=PLEF3AF2D56CBF2DC2"
     elif type == Genre.motivation:
-        target_playlist = "https://www.youtube.com/playlist?list=PLti4gdOly2nnCRZEibywyX9CoTiN2IZ5s"
+        target_playlist = "https://www.youtube.com/playlist?list=PLEF3AF2D56CBF2DC2"
     else:
         target_playlist = "https://www.youtube.com/playlist?list=PLti4gdOly2nnCRZEibywyX9CoTiN2IZ5s"
 
