eb28e8a826b0c1915684ac91978baafc7910bb87
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 04:11:09 -0700

    Update audio.py

--- src/audio.py
+++ src/audio.py
@@ -16,7 +16,7 @@ def download_mp3(type: Genre, id: int):
     elif type == Genre.motivation:
         target_playlist = "https://www.youtube.com/playlist?list=PLEF3AF2D56CBF2DC2"
     else:
-        target_playlist = "https://www.youtube.com/playlist?list=PLti4gdOly2nnCRZEibywyX9CoTiN2IZ5s"
+        target_playlist = "https://www.youtube.com/playlist?list=PLEF3AF2D56CBF2DC2"
 
     p = Playlist(target_playlist)
     required_video = p.videos[id]
