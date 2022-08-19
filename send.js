3a277e731018f1765955a58de2d8b5b45fd05a29
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 03:22:57 -0700

    Update send.js

--- send.js
+++ send.js
@@ -1,37 +1,25 @@
 import { Telegraf } from 'telegraf'
 import { createReadStream } from 'fs'
 import 'dotenv/config'
-
 var chatId = "-1709419186"
 let token = process.env.TELEGRAM_TOKEN
 let args = process.argv.slice(2)
 let bot = new Telegraf(token)
-
 let type = args[0]
 let src = args[1]
-
 if (type == "m") {
-
     console.log("Sending Message ...")
     bot.telegram.sendMessage(chatId, src)
-
 } else if (type == "v") {
-
     const readStream = createReadStream(src)
-
     bot.telegram.sendVideo(chatId, {
         source: readStream
     })
-
-
 } else if (type == "p") {
-
     const readStream = createReadStream(src)
-
     bot.telegram.sendVideo(chatId, {
         source: readStream
     })
-
 } else {
     bot.telegram.sendMessage("ok")
 }
