837ca224fd78c573406f890af10ba2a0ebb434d9
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 10:51:42 -0700

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -4,4 +4,5 @@ from moviepy.editor import *
 def concat_images_in_pictures_dir_to_video():
     os.system("ffmpeg -framerate 1/5 -pattern_type glob -i 'pictures/*.jpeg' -c:v libx264 -r 30 -pix_fmt yuv420p output_old.mp4")
 def add_output_mp3_audio_to_output_old_mp4():   
-    os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4")
+    os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output1.mp4")
+    os.system('ffmpeg -y -i output1.mp4 -i subscribe.png -filter_complex "overlay=x=main_w-overlay_w-(main_w*0.01):y=main_h-overlay_h-(main_h*0.01)" output.mp4')
