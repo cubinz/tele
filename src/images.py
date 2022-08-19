6b1a1a533e0c85209539209676b659c90133de4e
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 08:51:36 -0700

    Update images.py

--- src/images.py
+++ src/images.py
@@ -8,6 +8,13 @@ def peakpx(query: str) -> list[str]:
 
     return list(map(lambda tag: tag['data-src'], image_tags))
 
+def peakpx2(query: str) -> list[str]:
+    markup = requests.get(f"https://www.peakpx.com/en/search?q={query}").text
+    soup = BeautifulSoup(markup, "html.parser")
+    image_tags = soup.find_all("img", {'class', 'lazy lst_img'})
+
+    return list(map(lambda tag: tag['data-src'], image_tags))
+
 def wallpapercave(folder: str):
     markup = requests.get(f"https://wallpapercave.com/{folder}").text
     soup = BeautifulSoup(markup, "html.parser")
