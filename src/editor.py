1539af71be0429e0d18b06fc6c47e8153e80df38
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-19 08:18:45 -0700

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -5,7 +5,7 @@ def concat_images_in_pictures_dir_to_video():
     clips = [ImageClip(m).set_duration(3) for m in [f'./pictures/{x}' for x in os.listdir("pictures")]]
     concat_clip = concatenate_videoclips(clips)
     concat_clip.write_videofile("input1.mp4", fps=30)
-    clips = [ImageClip(m).set_duration(3) for m in [f'./pictures2/{x}' for x in os.listdir("pictures")]]
+    clips = [ImageClip(m).set_duration(3) for m in [f'./pictures2/{x}' for x in os.listdir("pictures2")]]
     concat_clip = concatenate_videoclips(clips)
     concat_clip.write_videofile("input2.mp4", fps=30)
 
