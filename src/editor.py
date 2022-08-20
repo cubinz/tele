082463a4d588eab8979b9c2ba561ce93ef456e44
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-20 22:08:15 +0800

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -7,8 +7,9 @@ def concat_images_in_pictures_dir_to_video():
     concat_clip.write_videofile("output_old.mp4", fps=24)
 
 def add_output_mp3_audio_to_output_old_mp4():
-    #os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output1.mp4")    
+    os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4")    
     os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/eff36db690d1798f20c0' -codec:a libmp3lame file1.mp3")
-    #os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/c3b670f18d9664c83d87' -codec:a libmp3lame file2.mp3")    
-    os.system("ffmpeg -i file1.mp3 -i output.mp3 -filter_complex amerge=inputs=2 -ac 2 output2.mp3")
-    os.system("ffmpeg -i output_old.mp4 -i output2.mp3 -c copy -map 0:v:0 -map 1:a:0 output1.mp4")   
+    os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/c3b670f18d9664c83d87' -codec:a libmp3lame file2.mp3")    
+    os.system("ffmpeg -i file1.mp3 -i file1.mp3 \
+                -filter_complex '[0:0][1:0]concat=n=2:v=0:a=1[out]' \
+                -map '[out]' meens.mp3") 
