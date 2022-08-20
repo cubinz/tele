47d47ca302784abab05d68474e8806d92cf10cdf
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-20 21:23:43 +0800

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -6,9 +6,8 @@ def concat_images_in_pictures_dir_to_video():
     concat_clip = concatenate_videoclips(clips)
     concat_clip.write_videofile("output_old.mp4", fps=24)
 
-def add_output_mp3_audio_to_output_old_mp4():
-    os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output2.mp4")
+def add_output_mp3_audio_to_output_old_mp4():    
     os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/eff36db690d1798f20c0' -codec:a libmp3lame file1.mp3")
     os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/c3b670f18d9664c83d87' -codec:a libmp3lame file2.mp3")    
-    os.system("ffmpeg -i output2.mp4 -i file1.mp3 -i file2.mp3 -c:v copy -map 0:v -map 1:a:0 -map 2:a:0 -y output.mp4")
+    os.system("ffmpeg -i output_old.mp4 -i output.mp3 -i file1.mp3 -i file2.mp3 -c copy -map 0:v:0 -map 1:a:0 -map 2:a:0 -map 3:a:0 output.mp4")
     
