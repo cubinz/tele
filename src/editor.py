722cfaa5a45fb4af732ff175730f3e44da9c758b
cubinz <111338817+cubinz@users.noreply.github.com>
2022-08-20 21:01:06 +0800

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -2,8 +2,11 @@ from moviepy.editor import *
 
 
 def concat_images_in_pictures_dir_to_video():
-    os.system("ffmpeg -framerate 1/5 -pattern_type glob -i 'pictures/*.jpeg' -c:v libx264 -r 30 -pix_fmt yuv420p output_old.mp4")
-def add_output_mp3_audio_to_output_old_mp4():   
+    clips = [ImageClip(m).set_duration(3) for m in [f'./pictures/{x}' for x in os.listdir("pictures")]]
+    concat_clip = concatenate_videoclips(clips)
+    concat_clip.write_videofile("output_old.mp4", fps=24)
+
+def add_output_mp3_audio_to_output_old_mp4():
     os.system("ffmpeg -i output_old.mp4 -i output.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4")
     os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/eff36db690d1798f20c0' -codec:a libmp3lame file1.mp3")
     os.system("ffmpeg -y -i 'https://chunk.lab.zalo.ai/c3b670f18d9664c83d87' -codec:a libmp3lame file2.mp3")
